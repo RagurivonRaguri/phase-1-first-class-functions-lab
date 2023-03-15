2fae7430f6536b3456346086ef22f4f8435a6d78
LincolnMuraguri <lincolnmuraguri93@gmail.com>
2023-03-15 20:59:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+const drivers=['Antonia', 'Nuru', 'Amari', 'Mo']
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+}
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2)
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+        return fare*integer;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers , returnLastTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+}
